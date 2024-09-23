482f3f3962dc9135ca04361de93b3826384d999d
Ravindu Wijegunawardhana <ravindu.w@gmail.com>
2024-09-23 19:30:19 +0530

    test implementation

--- tests/did_2018.rs
+++ tests/did_2018.rs
@@ -1,13 +1,13 @@
 use key_resolver::resolve_did;
 
-// #[test]
-pub fn test_did() {
-    let did = "did:key:z6MkpVCWpibzht7gFFkBsnNigRvXiQWQgV2vqq8eN8zGkGGN#z6MkpVCWpibzht7gFFkBsnNigRvXiQWQgV2vqq8eN8zGkGGN";
+#[test]
+pub fn test_did_key_1() {
+    let did = "did:key:z6MkpVCWpibzht7gFFkBsnNigRvXiQWQgV2vqq8eN8zGkGGN";
     let verification_key_public_key_base58 = "B2wUEUMZNLdD8kuVCDQsqLNXtqEZGbna9pDiXs2Fq3Uz";
     let agreement_key_public_key_base58 = "FTuUZW4g4pFAWjfreUWGyqGuV6iW6qfR7nmeEP4xRP5a";
     let suite = "Ed25519VerificationKey2018";
 
-    let (did_doc_option, key_pair_option) = match resolve_did(did, suite) {
+    let (did_doc_option, _) = match resolve_did(did, suite) {
         Ok(val) => (val.0, val.1),
         Err(error) => {
             eprintln!("{}", error.to_string());
@@ -18,6 +18,7 @@ pub fn test_did() {
 
     if did_doc_option.is_some() {
         let did_doc = did_doc_option.unwrap();
+        println!("{:#?}", did_doc);
 
         assert_eq!(
             did_doc.verification_method[0]
@@ -26,10 +27,8 @@ pub fn test_did() {
                 .expect("Public key base 58 not found"),
             verification_key_public_key_base58
         );
-    }
 
-    if key_pair_option.is_some() {
-        let key_pair = key_pair_option.unwrap();
+        let key_pair = &did_doc.key_agreement[0];
 
         assert_eq!(
             key_pair
@@ -39,6 +38,44 @@ pub fn test_did() {
             agreement_key_public_key_base58
         );
     }
+}
 
-    assert!(true)
+#[test]
+pub fn test_did_key_2() {
+    let did = "did:key:z6Mkr4zjFPqDsoQPo9t17N7pXo5oQXfjCFJ2bFDqpKnf9rBv";
+    let verification_key_public_key_base58 = "Ccjgf9anYFuvgf3JRo9yghXoaxPsnN3fuEJuz3peEdQY";
+    let agreement_key_public_key_base58 = "CbXfq23t36uB18TJJJJ6aAdAh5JpkrpoumS7mtLTAoTB";
+    let suite = "Ed25519VerificationKey2018";
+
+    let (did_doc_option, _) = match resolve_did(did, suite) {
+        Ok(val) => (val.0, val.1),
+        Err(error) => {
+            eprintln!("{}", error.to_string());
+            assert!(false);
+            return;
+        }
+    };
+
+    if did_doc_option.is_some() {
+        let did_doc = did_doc_option.unwrap();
+        println!("{:#?}", did_doc);
+
+        assert_eq!(
+            did_doc.verification_method[0]
+                .public_key_base58
+                .clone()
+                .expect("Public key base 58 not found"),
+            verification_key_public_key_base58
+        );
+
+        let key_pair = &did_doc.key_agreement[0];
+
+        assert_eq!(
+            key_pair
+                .public_key_base58
+                .clone()
+                .expect("Public key base 58 not found"),
+            agreement_key_public_key_base58
+        );
+    }
 }
--- tests/did_2020.rs
+++ tests/did_2020.rs
@@ -1,13 +1,13 @@
 use key_resolver::resolve_did;
 
 #[test]
-pub fn test_did() {
-    let did = "did:key:z6MkvEoFWxZ9B5RDGSTLo2MqE3YJTxrDfLLZyZKjFRtcUSyw#z6MkvEoFWxZ9B5RDGSTLo2MqE3YJTxrDfLLZyZKjFRtcUSyw";
-    let verification_key_public_key_multibase = "z6MkvEoFWxZ9B5RDGSTLo2MqE3YJTxrDfLLZyZKjFRtcUSyw";
-    let agreement_key_public_key_multibase = "z6LSjSGy9kLkMotj3zv1eQEYoo66LaBobzb5EabEhZh72wJQ";
+pub fn test_did_key_1() {
+    let did = "did:key:z6MkpTHR8VNsBxYAAWHut2Geadd9jSwuBV8xRoAnwWsdvktH";
+    let verification_key_public_key_multibase = "z6MkpTHR8VNsBxYAAWHut2Geadd9jSwuBV8xRoAnwWsdvktH";
+    let agreement_key_public_key_multibase = "z6LSbysY2xFMRpGMhb7tFTLMpeuPRaqaWM1yECx2AtzE3KCc";
     let suite = "Ed25519VerificationKey2020";
 
-    let (did_doc_option, key_pair_option) = match resolve_did(did, suite) {
+    let (did_doc_option, _) = match resolve_did(did, suite) {
         Ok(val) => (val.0, val.1),
         Err(error) => {
             eprintln!("{}", error.to_string());
@@ -18,27 +18,64 @@ pub fn test_did() {
 
     if did_doc_option.is_some() {
         let did_doc = did_doc_option.unwrap();
-        println!("{}", serde_json::to_string_pretty(&did_doc).unwrap());
+        println!("{:#?}", did_doc);
+
         assert_eq!(
             did_doc.verification_method[0]
                 .public_key_multibase
                 .clone()
-                .expect("Public key multi base not found"),
+                .expect("Public key multibase not found"),
             verification_key_public_key_multibase
         );
-    }
 
-    if key_pair_option.is_some() {
-        let key_pair = key_pair_option.unwrap();
+        let key_pair = &did_doc.key_agreement[0];
 
         assert_eq!(
             key_pair
                 .public_key_multibase
                 .clone()
-                .expect("Public key multi base not found"),
+                .expect("Public key multibase not found"),
             agreement_key_public_key_multibase
         );
     }
+}
 
-    assert!(true)
+#[test]
+pub fn test_did_key_2() {
+    let did = "did:key:z6MkvEoFWxZ9B5RDGSTLo2MqE3YJTxrDfLLZyZKjFRtcUSyw";
+    let verification_key_public_key_multibase = "z6MkvEoFWxZ9B5RDGSTLo2MqE3YJTxrDfLLZyZKjFRtcUSyw";
+    let agreement_key_public_key_multibase = "z6LSjSGy9kLkMotj3zv1eQEYoo66LaBobzb5EabEhZh72wJQ";
+    let suite = "Ed25519VerificationKey2020";
+
+    let (did_doc_option, _) = match resolve_did(did, suite) {
+        Ok(val) => (val.0, val.1),
+        Err(error) => {
+            eprintln!("{}", error.to_string());
+            assert!(false);
+            return;
+        }
+    };
+
+    if did_doc_option.is_some() {
+        let did_doc = did_doc_option.unwrap();
+        println!("{:#?}", did_doc);
+
+        assert_eq!(
+            did_doc.verification_method[0]
+                .public_key_multibase
+                .clone()
+                .expect("Public key multibase not found"),
+            verification_key_public_key_multibase
+        );
+
+        let key_pair = &did_doc.key_agreement[0];
+
+        assert_eq!(
+            key_pair
+                .public_key_multibase
+                .clone()
+                .expect("Public key multibase not found"),
+            agreement_key_public_key_multibase
+        );
+    }
 }
